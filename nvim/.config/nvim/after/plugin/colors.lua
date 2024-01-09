62f275f7952ba430f088254229e05b1f15e6adce
Amrit Twanabasu <amrit.cas@gmail.com>
2024-01-09 11:07:19 +0900

    fix: Match neotree and normal buffer colors

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -3,5 +3,14 @@
 vim.opt.termguicolors = true
 
 -- Change gruvbox style to match terminal colors
+
+-- Don't show end of buffers
+vim.g.gruvbox_material_show_eob=0
+
 vim.cmd.colorscheme("gruvbox-material")
 
+-- Change gruvbox-material x neo-tree config to match the colors between them
+vim.cmd([[
+highlight! link NeoTreeNormal Normal
+highlight! link NeoTreeEndOfBuffer NeoTreeVertSplit
+]])
