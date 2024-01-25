6887fc3fe035df2a124850bfa640a6e2dc4d40cf
Amrit Twanabasu <amrit.cas@gmail.com>
2024-01-25 15:01:10 +0900

    feat: Add java lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -35,6 +35,11 @@ require('mason-lspconfig').setup({
       local lua_opts = lsp_zero.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
     end,
+    jdtls = function ()
+      require('lspconfig').jdtls.setup({
+        root_dir = require('jdtls.setup').find_root({'.git', 'pom.xml', 'mvnw', 'gradlew'}),
+      })
+    end
   }
 })
 
